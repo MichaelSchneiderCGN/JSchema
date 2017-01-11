09d04f1e9b98e1a296badf8d5a09fdae12a1a3a8
Michael Schneider <schneider.michael.cgn@web.de>
2017-01-11 15:04:35 +0100

    Create netbeans project

---
+++ src/jschema/JSchema.java
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the DaSigned Framework.
+ * 
+ * (c) Michael Schneider <michael.schneider@dasined.de>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+package jschema;
+
+/**
+ *
+ * @author Michael Schneider <michael.schneider@dasined.de>
+ */
+public class JSchema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
